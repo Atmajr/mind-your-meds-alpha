202b86bb62561f4257d82737a42dd92abcc83dde
Justin Acker <justin@justinacker.com>
2017-10-07 14:04:10 +0000

    application_controller and config.ru skeleteons added'

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+require './config/environment'
+require 'rack-flash'
+class ApplicationController < Sinatra::Base
+  use Rack::Flash
+  register Sinatra::ActiveRecordExtension
+  enable :sessions
+  set :session_secret, "my_application_secret"
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+
+end
