91a7f026600abaf0ecf873599a2becaba86a4815
Justin Acker <justin@justinacker.com>
2017-10-07 14:28:08 +0000

    populate post action for signup in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,5 +19,37 @@ class ApplicationController < Sinatra::Base
     erb :'users/create_user'
   end
 
+  post '/signup' do
+
+    if (params[:username].blank?)
+      flash[:message] = 'Username cannot be blank.'
+      redirect to '/signup'
+    elsif (params[:password].blank?)
+      flash[:message] = 'Password cannot be blank.'
+      redirect to '/signup'
+    elsif (params[:email].blank?) #to do: validate e-mail format?
+      flash[:message] = 'Email cannot be blank.'
+      redirect to '/signup'
+    end
+
+    #to do: change condition to test these on the fly instead of setting to variables
+    @user_name_tester = User.find_by(username: params[:username])
+    @user_email_tester = User.find_by(email: params[:email])
+
+    if (@user_name_tester != nil)
+      flash[:message] = 'Username taken. Try another username.'
+      redirect to '/signup'
+    elsif (@user_email_tester != nil)
+      flash[:message] = 'The email is already registered. Log in, or use a different email address.'
+      redirect to '/signup'
+    end
+
+    @user = User.create(params)
+    puts @user
+    session[:user_id] = @user.id
+    redirect to '/'
+
+  end
+
 
 end
